be598f601c046c9f3775ecda9bd50b1093100dbc
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-15 11:55:12 +0100

    Add Main page, remove App.js

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">Display text</div>;
-}
-
-export default App;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,5 @@
+function Main() {
+  return <div className="App">Main works...</div>;
+}
+
+export default Main;
