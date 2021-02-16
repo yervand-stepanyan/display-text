09eb2f7a92e6526e75391b9b7817831d73f156ed
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-16 16:30:09 +0100

    Refactor component in Main.js

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,44 +1,55 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-import { BUTTON_LABEL, TITLE } from '../../globals/constants';
+import API from '../../fetchAPI';
+import ButtonGroup from '../../components/ButtonGroup';
+import ROUTES from '../../routes';
+import Text from '../../components/Text';
+import Title from '../../components/Title';
+import { TITLE } from '../../globals/constants';
 import { useStyles } from './Main.style';
 
 function Main() {
   const classes = useStyles();
-  const [showData, setShowData] = useState('');
-  const data = '{ message: “Hello World” }';
+  const [data, setData] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [messageToShow, setMessageToShow] = useState('');
 
-  const handleShow = () => {
-    setShowData(data);
+  useEffect(() => {
+    setMessageToShow(data.message || '');
+  }, [data]);
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+
+      const response = await API.getMessage(ROUTES.hello);
+
+      setData(response);
+    } catch (e) {
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleShow = async () => {
+    await getData();
   };
 
   const handleReset = () => {
-    setShowData('');
+    setData('');
   };
 
   return (
     <div className={classes.mainContainer}>
-      <div className={classes.titleContainer}>
-        <Typography variant="h4">{TITLE}</Typography>
-      </div>
-      <div className={classes.buttonsContainer}>
-        <div className={classes.showButtonWrapper}>
-          <Button color="primary" onClick={handleShow} variant="contained">
-            {BUTTON_LABEL.show}
-          </Button>
-        </div>
-        <div>
-          <Button color="secondary" onClick={handleReset} variant="outlined">
-            {BUTTON_LABEL.reset}
-          </Button>
-        </div>
-      </div>
-      <div className={classes.dataContainer}>
-        <Typography variant="h5">{showData}</Typography>
-      </div>
+      <Title title={TITLE} />
+      <ButtonGroup
+        handleReset={handleReset}
+        handleShow={handleShow}
+        isDisabled={!!data}
+        loading={loading}
+      />
+      <Text text={messageToShow} />
     </div>
   );
 }
--- src/pages/Main/Main.style.js
+++ src/pages/Main/Main.style.js
@@ -10,24 +10,4 @@ export const useStyles = makeStyles(theme => ({
     minHeight: '100vh',
     padding: theme.customSpacing.base,
   },
-  titleContainer: {
-    marginBottom: theme.customSpacing.large,
-    textAlign: 'center',
-  },
-  buttonsContainer: {
-    display: 'flex',
-    marginBottom: theme.customSpacing.large,
-  },
-  showButtonWrapper: {
-    marginRight: theme.customSpacing.large,
-  },
-  dataContainer: {
-    border: `1px solid ${theme.color.primaryColor}`,
-    borderRadius: theme.border.borderRadius,
-    boxSizing: 'border-box',
-    minHeight: theme.size.textFieldHeight,
-    minWidth: theme.size.textFieldWidth,
-    padding: `${theme.customSpacing.small} ${theme.customSpacing.base}`,
-    textAlign: 'center',
-  },
 }));
