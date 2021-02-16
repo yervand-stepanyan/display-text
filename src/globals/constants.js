cb5515ec97045e0aea9465343633847d29c9a16b
yervand-stepanyan <39864121+yervand-stepanyan@users.noreply.github.com>
2021-02-16 16:29:49 +0100

    Add Loader object in constants.js

--- src/globals/constants.js
+++ src/globals/constants.js
@@ -2,4 +2,10 @@ export const BUTTON_LABEL = {
   reset: 'Reset',
   show: 'Show',
 };
+export const LOADER = {
+  button: {
+    size: 24,
+    thickness: 8,
+  },
+};
 export const TITLE = `Click the "${BUTTON_LABEL.show}" button to get a Message`;
